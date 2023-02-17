9678b344a5a55b54b65798924fe48bb780bc4380
Mikha38 <oyks1379@yandex.ru>
2023-02-17 17:57:58 +0300

    OOP

--- KS3.js
+++ KS3.js
@@ -1,29 +1,51 @@
 const SHEET_NAME_KS3 = 'КС3'
 const COLUMS_COUNT_KS3 = 13
 
-function createScan(date, counterparty, contract, price){
-  const ss = SpreadsheetApp.openById(SS_ID)
-  const sheet = ss.getSheetByName(SHEET_NAME_KS3)
-  const lastRow = sheet.getLastRow()
-  sheet.insertRowAfter(lastRow)
-  const range = sheet.getRange(lastRow + 1, 1, 1, 5)
-  const rangeCheckbox = sheet.getRange(lastRow + 1, 6)
-  const id = sheet.getRange(lastRow, 1).getValue() + 1
-  range.setValues([
-    [id, date, contract, counterparty, price]
-  ])
-  rangeCheckbox.insertCheckboxes('ИСТИНА').check()
+class Ks3 extends Default{
+  constructor(sheet_name){
+    super(sheet_name)
+  }
 
-}
-function editScan() {
-  const ss = SpreadsheetApp.openById(SS_ID)
-  const sheet = ss.getSheetByName(SHEET_NAME_KS3)
-  const lastRow = sheet.getLastRow()
-  sheet.insertRowAfter(lastRow)
-}
-function rejectScan(id){
+  addScan(date, counterparty, contract, price){
+    const sheet = this.getSheet()
+    const last_row = sheet.getLastRow()
+    sheet.insertRowAfter(last_row)
+    const range = sheet.getRange(lastRow + 1, 1, 1, 5)
+    const rangeCheckbox = sheet.getRange(lastRow + 1, 6)
+    const id = sheet.getRange(lastRow, 1).getValue() + 1
+    range.setValues([
+      [id, date, contract, counterparty, price]
+    ])
+    rangeCheckbox.insertCheckboxes('ИСТИНА').check()
+  }
 
+  editScan(){}
+  rejectScan(id){}
 }
+
+// function createScan(date, counterparty, contract, price){
+//   const ss = SpreadsheetApp.openById(SS_ID)
+//   const sheet = ss.getSheetByName(SHEET_NAME_KS3)
+//   const lastRow = sheet.getLastRow()
+//   sheet.insertRowAfter(lastRow)
+//   const range = sheet.getRange(lastRow + 1, 1, 1, 5)
+//   const rangeCheckbox = sheet.getRange(lastRow + 1, 6)
+//   const id = sheet.getRange(lastRow, 1).getValue() + 1
+//   range.setValues([
+//     [id, date, contract, counterparty, price]
+//   ])
+//   rangeCheckbox.insertCheckboxes('ИСТИНА').check()
+
+// }
+// function editScan() {
+//   const ss = SpreadsheetApp.openById(SS_ID)
+//   const sheet = ss.getSheetByName(SHEET_NAME_KS3)
+//   const lastRow = sheet.getLastRow()
+//   sheet.insertRowAfter(lastRow)
+// }
+// function rejectScan(id){
+
+// }
 function getFolders(folder_id){
   const folder = DriveApp.getFolderById(folder_id)
 }
--- file.js
+++ file.js
@@ -1,3 +1,33 @@
+class File{
+  constructor(){}
+
+  setFile(file_id, folder_id, file_name){
+    const data = {
+    method: "post",
+    payload: {
+      method: "getFile",
+      file_id: String(file_id)
+      }
+    }
+    const response = UrlFetchApp.fetch(API_URL, data)
+    const url_document = API_FILE + JSON.parse(response).result.file_path
+    const blob_document = UrlFetchApp.fetch(url_document).getBlob().setName(file_name)
+    DriveApp.getFolderById(folder_id).createFile(blob_document)
+  }
+  getOrCreateFolderReturningId(parent_folder_id, inn){
+    const partner = partners.getNameByInn(inn) //Может вынести в дефаулт
+    const folders = DriveApp.getFolderById(parent_folder_id).getFolders()
+    while (folders.hasNext()) {
+      const folder = folders.next();
+      if(folder.getName() == partner){
+        return folder.getId()
+      }
+    }
+    return DriveApp.getFolderById(parent_folder_id).createFolder(partner).getId()
+    }
+}
+
+
 function setFileFromTG(file_id, folder_id, file_name){
   const data = {
     method: "post",
